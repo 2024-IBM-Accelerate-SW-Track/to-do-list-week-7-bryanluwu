eb022956e42df2eba57f8e992300d765c1a84242
Bryan Phillips <bryanphillips390@gmail.com>
2024-07-22 21:43:31 -0400

    finish

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,41 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import ibmpfp from "../assets/ibmpfp.JPG";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
+        <div>
+        <div className="split left">
+        <div className="centered">
+        <img 
+        className="profile_image"
+        src={ibmpfp}
+        alt="Profile Pic"
+        ></img>
+        </div>
         </div>
         <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+        <div className="centered">
+        <div className="name_title">Bryan Phillips</div>
+        <div className="brief_description">
+        <p>Hey! My name is Bryan, and I'm a rising junior at North Carolina State University studying computer science. 
+            I intend to learn as much as I can throughout this program, and I hope to be able to form bonds that will extend past this program too!
+        </p>
+        <p>Outside of just the usual computer science lifestyle, I love rock climbing, working out, eating good food, skating, and playing sports with friends. 
+            I also own a onewheel board that I particularly enjoy riding around campus with.
+        </p> 
+        <p>In terms of what I am interested in most in computer science, 
+            I would say I would like to indulge in full-stack development and would love to learn more about this particular aspect. 
+            Although most of my education has been catered more towards back-end development, I hope to be able to learn more about cloud, security, and front-end development 
+            throughout this program.
+        </p>
+        </div>
+        </div>
+        </div>
         </div>
       </div>
-    );
+    )
   }
 }
